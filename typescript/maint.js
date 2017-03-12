bdf7f9e0c31c2be1481da863165d4f7e816d6d99
Igor Elias <igor.elias.muniz@gmail.com>
2017-03-12 01:34:51 -0300

    basic tests using tsc transpiler

---
+++ typescript/maint.js
@@ -0,0 +1,17 @@
+var MathSum = (function () {
+    function MathSum() {
+    }
+    MathSum.prototype.sum = function (x, y) {
+        return x + y;
+    };
+    return MathSum;
+}());
+var numbers = [1, 2, 3];
+numbers.map(function (val) {
+    return val + 2;
+});
+var num = 12;
+var PI = 3.14;
+numbers.map(function (val) { return val + 2; });
+var n1 = 'kkk';
+n1 = 'rsrs';
---
+++ typescript/maint.ts
@@ -0,0 +1,18 @@
+class MathSum {
+    sum ( x, y ) {
+        return x + y;
+    }
+}
+
+var numbers = [1,2,3];
+numbers.map(function(val){
+    return val + 2
+});
+
+let num = 12;
+const PI = 3.14;
+
+numbers.map( (val) => val + 2)
+
+var n1: string = 'kkk';
+n1 = 'rsrs';
